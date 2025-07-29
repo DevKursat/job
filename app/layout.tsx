e8e39b9e0d771f0dc827501eb14cdc925d8edf52
DevKursat <lusid.help@gmail.com>
2025-07-30 02:28:57 +0300

    feat: Initial setup of Next.js project and GitHub Pages deployment

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,39 +1,14 @@
-import type { Metadata } from "next";
-import { Inter } from "next/font/google";
-import "./globals.css";
-import Head from "next/head";
+import './globals.css';
 
-const inter = Inter({ subsets: ["latin"] });
-
-export const metadata: Metadata = {
-  title: "Kürşat Yılmaz - Proje Başarı Ortağınız",
-  description: "Yazılım projelerinizde %100 başarı garantisi. Sadece bir freelancer değil, projenizin başarı ortağı.",
+export const metadata = {
+  title: 'Job Portfolio',
+  description: 'Modern portfolio for job seekers',
 };
 
-export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="tr">
-      <head>
-        <link
-            rel="stylesheet"
-            href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css"
-            integrity="sha512-SnH5WK+bZxgPHs44uWIX+LLJAJ9/2PkPKZ5QiAj6Ta86w+fsb2TkcmfRyVX3pBnMFcV7oQPJkl9QevSCWr3W6A=="
-            crossOrigin="anonymous"
-            referrerPolicy="no-referrer"
-          />
-      </head>
-      <body className={inter.className}>
-        {children}
-        <script
-            src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
-            integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"
-            crossOrigin="anonymous"
-        ></script>
-      </body>
+    <html lang="en">
+      <body>{children}</body>
     </html>
   );
 }
