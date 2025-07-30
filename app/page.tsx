f2eeea0b7b2ab9de0562ddab6fedaced0eac831c
DevKursat <lusid.help@gmail.com>
2025-07-30 15:03:52 +0300

    fix: Center scroll indicator and confirm carousel arrows removal

--- app/page.tsx
+++ app/page.tsx
@@ -277,7 +277,7 @@ export default function Home() {
         </div>
         {/* Scroll Down Indicator */}
         <motion.div
-          className="absolute bottom-8 left-1/2 -translate-x-1/2 w-full flex flex-col items-center text-gray-400"
+          className="absolute bottom-8 flex flex-col items-center text-gray-400 mx-auto"
           initial={{ opacity: 0, y: 20 }}
           animate={{ opacity: 1, y: 0 }}
           transition={{ repeat: Infinity, duration: 1.5, ease: "easeInOut" }}
